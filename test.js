8b728c4d8a178d2f4c869ea427a09b38008abce4
nook <nookittipon@gmail.com>
2019-06-03 09:44:13 +0700

    add catch

--- test.js
+++ test.js
@@ -3,9 +3,12 @@ let Emails = require('./lib/index')
 
 const test = async () => {
     var e = new Emails()
-
-    const res = await e.verify('ittipon@mycostech.com')
-    console.log(res)
+    try {
+     const res = await e.verify('ittipon@mycostech.com') 
+     console.log(res)
+   } catch(err) {
+    console.log(err)
+    }
 }
 
 test()
